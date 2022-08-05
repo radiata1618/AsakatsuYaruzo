3fb0fb8eb7c59c3048cabd920c3fe22ce8822031
haruk <haruki.u@gmail.com>
2022-08-06 01:05:15 +0900

    メイン画面のリスト表示を作成中

--- app/src/main/java/com/app/asakatsuyaruzo/MainActivity.kt
+++ app/src/main/java/com/app/asakatsuyaruzo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.app.asakatsuyaruzo
 
+import android.app.Application
 import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,20 +11,52 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Observer
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.room.Room
+import com.app.asakatsuyaroze.data.AlarmPattern
+import com.app.asakatsuyaroze.data.AlarmPatternDao
+import com.app.asakatsuyaroze.data.AppDatabase
+import com.app.asakatsuyaruzo.ui.theme.AlarmPatternList
 import com.app.asakatsuyaruzo.ui.theme.AsakatsuYaruzoTheme
 import com.app.asakatsuyaruzo.ui.theme.ViewAlarmPatternUI
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        lateinit var database: AppDatabase
+        lateinit var alarmPatternDao: AlarmPatternDao
+        var mainAlarmPatternList = mutableStateListOf<AlarmPattern>()
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        database = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "main-database"
+        ).build()
+
+        alarmPatternDao = database.alarmPatternDao()
+
+        alarmPatternDao.getAllLiveData().observe(this, Observer{
+            mainAlarmPatternList.clear()
+            mainAlarmPatternList.addAll(it)
+            Toast.makeText(applicationContext, mainAlarmPatternList.size.toString()+"koko", Toast.LENGTH_LONG).show()
+//            AlarmPatternList(alarmPatternDataList = it, navController = navController)
+        })
+
         setContent {
             MainScreenUI()
         }
--- app/src/main/java/com/app/asakatsuyaruzo/ViewAlarmPatternUI.kt
+++ app/src/main/java/com/app/asakatsuyaruzo/ViewAlarmPatternUI.kt
@@ -1,105 +1,61 @@
 package com.app.asakatsuyaruzo.ui.theme
 
+import android.annotation.SuppressLint
+import android.app.Application
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
+import androidx.room.Room
 import com.app.asakatsuyaroze.data.AlarmPattern
+import com.app.asakatsuyaroze.data.AppDatabase
+import com.app.asakatsuyaruzo.MainActivity.Companion.alarmPatternDao
+import com.app.asakatsuyaruzo.MainActivity.Companion.database
+import com.app.asakatsuyaruzo.MainActivity.Companion.mainAlarmPatternList
+import kotlinx.coroutines.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ViewAlarmPatternUI(navController: NavController) {
+
+
     Scaffold(floatingActionButton = { MainFloatingActionButton(navController) }) {
-        AlarmPatternList()
+
+        AlarmPatternList(navController)
     }
 }
+//
+//private fun loadAlarmPatternDataList() {
+//    CoroutineScope(Dispatchers.Main).launch {
+//        withContext(Dispatchers.Default) {
+//            alarmPatternDao.getAll().forEach { todo ->
+//                alarmPatternDataList.add(todo)
+//            }
+//        }
+//    }
+//}
 
 @Composable
-fun LazyColumn(
-    modifier: Modifier = Modifier,
-    state: LazyListState = rememberLazyListState(),
-    contentPadding: PaddingValues = PaddingValues(0.dp),
-    reverseLayout: Boolean = false,
-    verticalArrangement: Arrangement.Vertical =
-        if (!reverseLayout) Arrangement.Top else Arrangement.Bottom,
-    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
-    flingBehavior: FlingBehavior = ScrollableDefaults.flingBehavior(),
-    content: LazyListScope.() -> Unit
-) {
-}
-https://engawapg.net/jetpack-compose/1442/list/
-@Composable
-fun AlarmPatternList() {
-    LazyColumn{
-        item {
+fun AlarmPatternList(navController: NavController) {
+    LazyColumn {
+        items(mainAlarmPatternList) { alarmPatternData ->
             AlarmPatternListUnit(
-                AlarmPattern(
-                    0,
-                    "namename",
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    0,
-                    0,
-                    false
-                )
+                alarmPatternData
             )
-
-        }
-        item {
-            AlarmPatternListUnit(
-                AlarmPattern(
-                    0,
-                    "name2",
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    0,
-                    0,
-                    false
-                )
-            )
-
-        }
-        item {
-            AlarmPatternListUnit(
-                AlarmPattern(
-                    0,
-                    "name3",
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    false,
-                    0,
-                    0,
-                    false
-                )
-            )
-
         }
     }
 }
