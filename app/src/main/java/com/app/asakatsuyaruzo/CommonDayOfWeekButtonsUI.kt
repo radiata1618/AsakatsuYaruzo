17beb096e742843373836ba96eaa167ec5f2ccfb
haruk <haruki.u@gmail.com>
2022-08-07 17:36:42 +0900

    曜日ボタンの修正完了

--- app/src/main/java/com/app/asakatsuyaruzo/CommonDayOfWeekButtonsUI.kt
+++ app/src/main/java/com/app/asakatsuyaruzo/CommonDayOfWeekButtonsUI.kt
@@ -1,19 +1,26 @@
 package com.app.asakatsuyaruzo
 
-import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.app.asakatsuyaroze.data.AlarmPattern
 import com.app.asakatsuyaruzo.common.CommonSpaceBasicHorizontal6
+import com.app.asakatsuyaruzo.ui.theme.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -40,73 +47,71 @@ fun CommonDayOfWeekButtons(alarmPattern: AlarmPattern) {
 
 @Composable
 fun DayOfWeekButtonUnit(dayName: String,alarmPattern: AlarmPattern) {
-    var buttonColor: Color
-    var buttonEnable:Boolean=false
+    var selected:Boolean=false
     when(dayName){
-        "monday" -> buttonEnable=alarmPattern.monday
-        "tuesday" -> buttonEnable=alarmPattern.tuesday
-        "wednesday" -> buttonEnable=alarmPattern.wednesday
-        "thursday" -> buttonEnable=alarmPattern.thursday
-        "friday" -> buttonEnable=alarmPattern.friday
-        "saturday" -> buttonEnable=alarmPattern.saturday
-        "sunday" -> buttonEnable=alarmPattern.sunday
+        "monday" -> selected=alarmPattern.monday
+        "tuesday" -> selected=alarmPattern.tuesday
+        "wednesday" -> selected=alarmPattern.wednesday
+        "thursday" -> selected=alarmPattern.thursday
+        "friday" -> selected=alarmPattern.friday
+        "saturday" -> selected=alarmPattern.saturday
+        "sunday" -> selected=alarmPattern.sunday
     }
 
-    Log.d("D/■■■■■■■■■■■■■■■■■■■■■■■■■AADDD",buttonEnable.toString())
-    Log.d("■■■■■■■■■■■■■111あいでぃー",alarmPattern.id.toString())
-
-    if(buttonEnable){
-        buttonColor= Color.Red
-    }else{
-        buttonColor = Color.Gray
-
-    }
-
-    Button(
+    Surface(
         modifier = Modifier
             .size(30.dp, 30.dp)
-            .padding(all = 0.dp),
-        shape = RoundedCornerShape(100),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = buttonColor
-        ),
-        onClick = {
-            var monday:Boolean?=alarmPattern.monday
-            var tuesday:Boolean?=alarmPattern.tuesday
-            var wednesday:Boolean?=alarmPattern.wednesday
-            var thursday:Boolean?=alarmPattern.thursday
-            var friday:Boolean?=alarmPattern.friday
-            var saturday:Boolean?=alarmPattern.saturday
-            var sunday:Boolean?=alarmPattern.sunday
-            when(dayName) {
-                "monday" -> monday=!alarmPattern.monday
-                "tuesday" -> tuesday=!alarmPattern.tuesday
-                "wednesday" -> wednesday=!alarmPattern.wednesday
-                "thursday" -> thursday=!alarmPattern.thursday
-                "friday" -> friday=!alarmPattern.friday
-                "saturday" -> saturday=!alarmPattern.saturday
-                "sunday" -> sunday=!alarmPattern.sunday
-            }
+            .toggleable(
+                value = selected,
+                onValueChange = {
+                    var monday:Boolean?=alarmPattern.monday
+                    var tuesday:Boolean?=alarmPattern.tuesday
+                    var wednesday:Boolean?=alarmPattern.wednesday
+                    var thursday:Boolean?=alarmPattern.thursday
+                    var friday:Boolean?=alarmPattern.friday
+                    var saturday:Boolean?=alarmPattern.saturday
+                    var sunday:Boolean?=alarmPattern.sunday
+                    when(dayName) {
+                        "monday" -> monday=!alarmPattern.monday
+                        "tuesday" -> tuesday=!alarmPattern.tuesday
+                        "wednesday" -> wednesday=!alarmPattern.wednesday
+                        "thursday" -> thursday=!alarmPattern.thursday
+                        "friday" -> friday=!alarmPattern.friday
+                        "saturday" -> saturday=!alarmPattern.saturday
+                        "sunday" -> sunday=!alarmPattern.sunday
+                    }
 
-            GlobalScope.launch(Dispatchers.IO) {
-                MainActivity.alarmPatternDao.updateDefault(
-                    alarmPattern.id,
-                    null,
-                    monday,
-                    tuesday,
-                    wednesday,
-                    thursday,
-                    friday,
-                    saturday,
-                    sunday,
-                    null,
-                    null,
-                    null,
-                    "dayOfWeek",
-                )
-            }
-        },
+                    GlobalScope.launch(Dispatchers.IO) {
+                        MainActivity.alarmPatternDao.updateDefault(
+                            alarmPattern.id,
+                            null,
+                            monday,
+                            tuesday,
+                            wednesday,
+                            thursday,
+                            friday,
+                            saturday,
+                            sunday,
+                            null,
+                            null,
+                            null,
+                            "dayOfWeek",
+                        )
+                    }
+                }
+            ),
+        shape = CircleShape,
+        color = if (selected) CommonColorSecondary else CommonColorTertiary,
     ) {
-        Text(dayName.substring(0, 1).uppercase())
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = dayName.substring(0, 1).uppercase(),
+                fontWeight = FontWeight.Bold,
+                fontSize = 15.sp,
+                color = if (selected) Color.White else Color.Black,
+            )
+        }
     }
 }
